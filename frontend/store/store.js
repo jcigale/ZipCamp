2fcf4fa355a05bdc6bb504d9c3127e4495a3d734
John Cigale <jcigale98@gmail.com>
2021-06-02 14:45:12 -0400

    middleware

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -14,7 +14,7 @@ const configureStore = (preloadedState={}) => (
     createStore(
         rootReducer,
         preloadedState,
-        applyMiddleware(middlewares)
+        applyMiddleware(...middlewares)
     )
 );
 
