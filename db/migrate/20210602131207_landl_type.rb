ebc0f9e4bf8b51b7ee5f3c8786c8707bc5de5f76
John Cigale <jcigale98@gmail.com>
2021-06-02 11:07:23 -0400

    remove migration

--- db/migrate/20210602131207_landl_type.rb
+++
@@ -1,7 +0,0 @@
-class LandlType < ActiveRecord::Migration[5.2]
-  def change
-    change_column :spots, :longitude, :integer, using: 'longitude::integer'
-    change_column :spots, :latitude, :integer, using: 'latitude::integer'
-
-  end
-end
