51f2e36454ac0e82d30c95fd2d95ea10e362b113
John Cigale <jcigale98@gmail.com>
2021-06-02 11:10:54 -0400

    migration

--- db/migrate/20210602132451_lng_lat_float.rb
+++ db/migrate/20210602132451_lng_lat_float.rb
@@ -1,8 +1,8 @@
 class LngLatFloat < ActiveRecord::Migration[5.2]
   def change
 
-    change_column :spots, :longitude, :float 
-    change_column :spots, :latitude, :float
+    change_column :spots, :longitude, :float, "USING longitude::double precision" 
+    change_column :spots, :latitude, :float, "USING longitude::double precision"
 
   end
 end
