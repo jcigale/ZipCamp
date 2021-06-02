640233b02910381a0cf73e586fbff36b76cf21dd
John Cigale <jcigale98@gmail.com>
2021-06-02 11:25:01 -0400

    alteer

--- db/migrate/20210602132451_lng_lat_float.rb
+++ db/migrate/20210602132451_lng_lat_float.rb
@@ -1,8 +1,8 @@
 class LngLatFloat < ActiveRecord::Migration[5.2]
   def change
 
-    change_column :spots, :longitude, :float
-    change_column :spots, :latitude, :float
+    ALTER TABLE spots ALTER longitude TYPE float USING longitude::float;
+    ALTER TABLE spots ALTER latitude  TYPE float USING latitude::float;
 
   end
 end
